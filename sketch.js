3575752a07d8e983db4f29f6c5aa0f5d92411914
gsprashaanth4 <77629690+gsprashaanth4@users.noreply.github.com>
2021-06-23 23:20:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1132,7 +1132,7 @@ function draw()
 
   //positions for all buttons...
   skip.position(920,700);
-  nameInput.position(200,657);
+  nameInput.position(200,357);
   enter.position(500, displayHeight/2+ 50);
   startNext.position(920,700);
   settingsss.position(920,60);
